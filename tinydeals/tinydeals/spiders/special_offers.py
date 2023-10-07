06684072f4890730f9c1f2e12615249ea5fde476
Marco <james.marco.reyes@eee.upd.edu.ph>
2023-10-07 16:52:44 +0800

    Build special offer spider and export dataset

--- tinydeals/tinydeals/spiders/special_offers.py
+++ tinydeals/tinydeals/spiders/special_offers.py
@@ -4,7 +4,13 @@ import scrapy
 class SpecialOffersSpider(scrapy.Spider):
     name = "special_offers"
     allowed_domains = ["web.archive.org"]
-    start_urls = ["http://web.archive.org/"]
+    start_urls = ["https://web.archive.org/web/20190225123327/https://www.tinydeal.com/specials.html"]
 
     def parse(self, response):
-        pass
+        for product in response.xpath("//ul[@class='productlisting-ul']/div/li"):
+            yield{
+                'title': product.xpath("./a[@class='p_box_title']/text()").get(),
+                'url': response.urljoin(product.xpath(".//a[@class='p_box_title']/@href").get()), #url is relative url so urljoin() must be used
+                'discounted_price': product.xpath(".//div[@class='p_box_price']/span[1]/text()").get(),
+                'original_price': product.xpath(".//div[@class='p_box_price']/span[2]/text()").get(), 
+            }
