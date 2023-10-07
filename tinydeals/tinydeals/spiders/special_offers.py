79b259803a8844a7c695ae477736d98a56d7c326
Marco <james.marco.reyes@eee.upd.edu.ph>
2023-10-07 17:41:07 +0800

    Scrape through entering the next page (pagination)

--- tinydeals/tinydeals/spiders/special_offers.py
+++ tinydeals/tinydeals/spiders/special_offers.py
@@ -14,3 +14,8 @@ class SpecialOffersSpider(scrapy.Spider):
                 'discounted_price': product.xpath(".//div[@class='p_box_price']/span[1]/text()").get(),
                 'original_price': product.xpath(".//div[@class='p_box_price']/span[2]/text()").get(), 
             }
+            
+        next_page = response.xpath("//a[@class='nextPage']/@href").get()
+        
+        if next_page:
+            yield scrapy.Request(url=next_page, callback=self.parse)
